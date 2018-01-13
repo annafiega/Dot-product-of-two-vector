2281f9658e403fade7399ddec9afeee7da75add7
annafiega <annafiega@gmail.com>
2018-01-13 20:45:16 +0100

    test

--- src/main/java/DotProduct.java
+++ src/main/java/DotProduct.java
@@ -2,7 +2,7 @@ public class DotProduct {
 
     public static void main(String[] args) {
         int[] vectorA = {1, 2, 3};
-        int[] vectorB = {4, 5, 6};
+        int[] vectorB = {4, 5, 5};
 
         int product = 0;
         for (int i = 0; i < vectorA.length; i++) {
